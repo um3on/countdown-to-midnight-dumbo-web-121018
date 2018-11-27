8fea54fde8ec1e1526e4a3fec2f67c4cae531134
M. T <mariam.tamimi@gmail.com>
2018-11-27 02:02:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number >= 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+end
